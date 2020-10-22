76d65f0940c72c6d57d156c1666835e56fddfb81
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-10-22 13:18:46 -0700

    add shoutbox component

---
+++ src/components/ShoutBox.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { Card, CardContent, Divider, Typography } from '@material-ui/core';
