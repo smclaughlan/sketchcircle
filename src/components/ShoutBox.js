3eef93d9f0d0b6360466390e7feea6eb77261391
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-10-22 13:24:53 -0700

    test shoutbox setup

--- src/components/ShoutBox.js
+++ src/components/ShoutBox.js
@@ -2,3 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { Card, CardContent, Divider, Typography } from '@material-ui/core';
+
+const ShoutBox = props => {
+
+  return (
+    <Card>
+      <ul id="chatMessages">
+        <li>Message</li>
+        <li>Message 2</li>
+      </ul>
+      <form action="">
+        <input autoComplete="off" />
+        <button>Send</button>
+      </form>
+    </Card>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    token: state.user.token,
+    currentUserId: state.user.currentUserId,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(
+  ShoutBox
+);
