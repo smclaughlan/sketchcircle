3942b542b56d6326429bed38706d9f88e9769850
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-10-23 15:32:41 -0700

    show loading message in shoutbox

--- src/components/ShoutBox.js
+++ src/components/ShoutBox.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { Card, CardContent, Divider, Typography } from '@material-ui/core';
+import { apiBaseUrl } from '../config';
 
 const ShoutBox = props => {
+  const [chatMessages, setChatMessages] = React.useState();
 
   return (
     <Card style={{ margin: "5px auto", maxWidth: "300px" }} variant="outlined">
       <CardContent>
-        <ul id="chatMessages">
-          <li>Message</li>
-          <li>Message 2</li>
+        <ul id="chatMessages" style={{ listStyleType: "none" }}>
+          {chatMessages ?
+            <li>Loaded!</li> :
+            <li>Loading...</li>}
         </ul>
         <form action="">
           <input autoComplete="off" />
