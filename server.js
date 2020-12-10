c6033a9f9dde598509d4bd80ea7e07570f3eddcc
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-12-10 14:20:07 -0800

    Comment out all socketio related code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const io = require('socket.io')(http);
 
 
 app.use(favicon(__dirname + '/build/favicon.ico'));
@@ -18,22 +18,23 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
-io.on('connection', (client) => {
-  console.log('user connected');
-  io.emit('new connection', client.id);
 
-  client.on('chat message', (msg) => {
-    console.log('message: ' + msg.body);
-    //io.emit sends an event to everyone
-    //while socket.broadcast.emit() will send to everyone except emitting socket
-    io.emit('chat message', msg);
-  })
+// io.on('connection', (client) => {
+//   console.log('user connected');
+//   io.emit('new connection', client.id);
 
-  client.on('disconnect', () => {
-    console.log(`${client.id} user disconnected`);
-    io.emit('lost connection', client.id);
-  });
-});
+//   client.on('chat message', (msg) => {
+//     console.log('message: ' + msg.body);
+//     //io.emit sends an event to everyone
+//     //while socket.broadcast.emit() will send to everyone except emitting socket
+//     io.emit('chat message', msg);
+//   })
+
+//   client.on('disconnect', () => {
+//     console.log(`${client.id} user disconnected`);
+//     io.emit('lost connection', client.id);
+//   });
+// });
 
 http.listen(port, () => {
   console.log('listening on ', port);
