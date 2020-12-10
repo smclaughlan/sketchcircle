7cf8e603d5efe953ecaa8054f8563059efd6c13b
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-12-10 14:08:22 -0800

    Remove unused imports from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Typography } from '@material-ui/core';
